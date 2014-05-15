96182fd5f172d253c882c5541dd05f468c8e842f
Max Ushakov <ushmax@gmail.com>
2014-05-15 08:49:52 +0400

    use point clouds instead of lines to increase performance

--- levels.py
+++ levels.py
@@ -29,82 +29,12 @@ def seq(pl):
         ret.append([p.x, p.y, p.z])
     return ret
 
-def drawLoops(myscreen, loops, loopcolor):
-    nloop = 0
-    for lop in loops:
-        n = 0
-        N = len(lop)
-        first_point=ocl.Point(-1,-1,5)
-        previous=ocl.Point(-1,-1,5)
-        for p in lop:
-            if n==0: # don't draw anything on the first iteration
-                previous = p 
-                first_point = p
-            elif n == (N-1): # the last point
-                myscreen.addActor( camvtk.Line(p1=(previous.x,previous.y,previous.z),p2=(p.x,p.y,p.z),color=loopcolor) ) # the normal line
-                # and a line from p to the first point
-                myscreen.addActor( camvtk.Line(p1=(p.x,p.y,p.z),p2=(first_point.x,first_point.y,first_point.z),color=loopcolor) )
-            else:
-                myscreen.addActor( camvtk.Line(p1=(previous.x,previous.y,previous.z),p2=(p.x,p.y,p.z),color=loopcolor) )
-                previous=p
-            n=n+1
-        print "rendered loop ",nloop, " with ", len(lop), " points"
-        if len(lop) == 2:
-            for p in lop:
-                print p
-                myscreen.addActor( camvtk.Sphere(center=(p.x,p.y,p.z),radius=0.1,color=camvtk.green) )
-        nloop = nloop+1
-
-# def drawLoops(myscreen, loops, color):
-#     points = []
-#     for loop in loops:
-#         for p in loop:
-#             points.append(p)
-#     myscreen.addActor(camvtk.PointCloud(points))
-
-kSimplifyTolerance = 0.2
-
-def Simplify(plist):
-    ret = []
-    horizon = 5
-    i = 0
-    skips = 0
-    maxskip = 0
-    ret.append(plist[0])
-    # inv.: i'th point is added to the ret list
-    while i + horizon < len(plist):
-        p1 = plist[i]
-        p2 = plist[i + horizon]
-        v = p2 - p1
-        v.normalize()
-        good = True
-        for p in plist[i:i+horizon]:
-            r = p - p1
-            dp = r.dot(v)
-            if dp > kSimplifyTolerance:
-                good = False
-        if not good:
-            ret.append(plist[i+1])
-            i += 1
-            continue
-        f = i + horizon
-        while good and f + 1 < len(plist):
-            r = plist[f+1] - p1
-            dp = r.dot(v)
-            if dp > kSimplifyTolerance:
-                good = False
-            else:
-                f += 1
-        ret.append(plist[f])
-        maxskip = max(maxskip, f - i)
-        skips += 1
-        i = f
-    i += 1
-    while i < len(plist):
-        ret.append(plist[i])
-        i += 1
-    print "Simplify: %d->%d, skips=%d maxskip=%d" % (len(plist),len(ret),skips,maxskip)
-    return ret
+def drawLoops(myscreen, loops, color):
+    points = []
+    for loop in loops:
+        for p in loop:
+            points.append(p)
+    myscreen.addActor(camvtk.PointCloud(points))
             
 kTop = 14
 kBottom = 0
@@ -171,8 +101,7 @@ if __name__ == "__main__":
         wl.setZ(l + 0.001)
         wl.run2()
         for lp in wl.getLoops():
-            simple = Simplify(lp)
-            all_loops.append(simple)
+            all_loops.append(lp)
 
     print "levels:", [ "%s" % l for l in essential_levels ]
     
